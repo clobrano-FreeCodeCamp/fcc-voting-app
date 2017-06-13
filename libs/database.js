ddee1a74802f3fb637e73b7cb403062c1faa242c
Carlo Lobrano <c.lobrano@gmail.com>
2017-06-13 17:24:39 +0200

    database: added remote mongodb url

--- libs/database.js
+++ libs/database.js
@@ -1,8 +1,14 @@
 var mongodb = require('mongodb');
 var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/test';
+var user = process.env.MONGO_USER;
+var pass = process.env.MONGO_PASSWORD;
 
+if (user && pass) {
+    var url = 'mongodb://' + user + ':' + pass + '@ds111262.mlab.com:11262/voting-app';
+} else {
+    var url = 'mongodb://localhost:27017/test';
+}
 
 module.exports.on_connect = function on_connect(action, userdata, cbk) {
     mongodb.connect(url, (err, db) => {
